2f12d5d28b07ce26a877bee36f28f4961364d748
ChristopherPapanicolas87 <cpapanic1@yahoo.com>
2017-02-04 22:52:08 -0500

    R scripts for plots

---
+++ plot1.R
@@ -0,0 +1,16 @@
+##Read the files that will be analyzed 
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
+# Using the base plotting system, make a plot showing the total PM2.5 emission from all sources 
+# for each of the years 1999, 2002, 2005, and 2008.
+
+#Aggregate the Data 
+aggregatedTotalByYear <- aggregate(Emissions ~ year, NEI, sum)
+
+#Create Plot 
+png('plot1.png')
+barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' emissions at various years'))
+dev.off()
